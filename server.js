4c634cb87afcb58323673809567a3c4b8dfab3fc
Dev-LucasMelo <lmelo072002@gmail.com>
2022-02-07 23:04:37 -0300

    deploy stts

--- server.js
+++ server.js
@@ -12,6 +12,4 @@ app.use(express.static(__dirname + '/public'))
 app.use('/', require('./routes/Routes') )
 
 
-app.listen(process.env.PORT || 4000, ()=>{
-    console.log("Servidor rodando na porta 4000")
-})
+app.listen(process.env.PORT || 4000)
