85eff956e37580b2b97321269e1082a2dded55fe
Dev-LucasMelo <lmelo072002@gmail.com>
2022-02-08 15:40:26 -0300

    produção

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const pg = require('pg')
 
-var uri_local = 'postgres://postgres:Lm07112002@localhost:5432/teste'
+
 
 const database = new pg.Client(process.env.DATABASE_URL)
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express') 
 const bodyparser = require('body-parser')
 const cors = require('cors')
+const path = require('path')
 require('dotenv/config')
 const app = express()
 
