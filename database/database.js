9103c7a8c54a68867e4594df2879ccc79bcb0a34
Dev-LucasMelo <lmelo072002@gmail.com>
2022-02-09 10:56:13 -0300

    git circleci

--- database/database.js
+++ database/database.js
@@ -1,7 +1,8 @@
 const pg = require('pg')
 
-
+var uri_local = 'postgres://postgres:Lm07112002@localhost:5432/teste'
 
 const database = new pg.Client(process.env.DATABASE_URL)
+database.connect() //AGUARDANDO CONEXÂO DO BANCO DE DADOS
 
 module.exports = database
--- routes/Routes.js
+++ routes/Routes.js
@@ -3,12 +3,10 @@ const Router = express.Router()
 const database = require('../database/database')
 
 
-async function connect() {
-    await database.connect() //AGUARDANDO CONEXÂO DO BANCO DE DADOS
     //GET 
 
     Router.get('/dados', (req, res) => { // ROTA COM OS DADOS RETORNADOS DA PROMISE "Dados.rows" = linhas do banco de dados "teste"  
-        var sql = database.query('select * from users order by id')
+        var sql = database.query('select * from users')
         sql.then((values) => {
             res.json(values.rows)
         })
@@ -18,21 +16,11 @@ async function connect() {
 
     Router.post('/finalizado', (req, res) => {
 
-        database.query('insert into users (name,about,age,sex) values ($1,$2,$3,$4)', [req.body.name, req.body.about, req.body.age, req.body.sexo])
+        database.query('insert into users (name,about,age,sex) values ($1,$2,$3,$4)', [req.body.name, req.body.about, req.body.age, req.body.sex])
 
         res.sendFile(__dirname + '/html/index.html')
 
     })
-}
-
-connect()
-
-
-
-
-
-
-
 
 
 module.exports = Router
