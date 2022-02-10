5068e87a884b39b04ff28327c5832c19c32ab121
Dev-LucasMelo <lmelo072002@gmail.com>
2022-02-09 21:04:10 -0300

    correção de falha no deploy

--- database/database.js
+++ database/database.js
@@ -2,7 +2,7 @@ const pg = require('pg')
 
 var uri_local = 'postgres://postgres:Lm07112002@localhost:5432/teste'
 
-const database = new pg.Client(process.env.DATABASE_URL)
+const database = new pg.Client(process.env.DATABASE_URL || uri_local)
 database.connect() //AGUARDANDO CONEXÂO DO BANCO DE DADOS
 
 module.exports = database
